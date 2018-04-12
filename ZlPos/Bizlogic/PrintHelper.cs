95a51e8f466347e630f1642b9a6b5b301b32f98f
sVen <BraveShining@gmail.com>
2018-04-12 10:54:48 +0800

    add printHelper.cs

---
+++ ZlPos/Bizlogic/PrintHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZlPos.Bizlogic
+{
+    //这个类用来桥接底层打印
+    public static class PrintHelper
+    {
+        
+    }
+}
