f82e0a2c7e7f65e02d89541c26c0c68ddcd801fa
sVen <BraveShining@gmail.com>
2018-05-08 19:51:29 +0800

    增加用户权限启动（admin）

---
+++ ZlPos/Utils/ModelComperar.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZlPos.Utils
+{
+    //public class ModelComperar<T> : IEqualityComparer<T>
+    //{
+    //    public bool Equals(T x, T y)
+    //    {
+    //        if (Object.ReferenceEquals(x, y)) return true;
+
+            
+
+    //    }
+
+    //    public int GetHashCode(T obj)
+    //    {
+    //        return 0;
+    //    }
+    //}
+}
